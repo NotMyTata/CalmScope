2d3bddb31c0dd87210831a0b6b2926363c461960
NotMyTata <161783398+NotMyTata@users.noreply.github.com>
2024-12-11 07:27:05 +0700

    Connect database to photo

--- app/src/main/java/com/example/calmscope/Photo/NeutralFragment.java
+++ app/src/main/java/com/example/calmscope/Photo/NeutralFragment.java
@@ -1,5 +1,7 @@
 package com.example.calmscope.Photo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -15,6 +17,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.calmscope.CalmDatabase.CalmDB;
+import com.example.calmscope.CalmDatabase.Entities.Results;
 import com.example.calmscope.R;
 
 public class NeutralFragment extends Fragment {
@@ -22,6 +26,14 @@ public class NeutralFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_result, container, false);
+
+        CalmDB db = CalmDB.getInstance(getContext());
+        SharedPreferences prefs = getContext().getSharedPreferences("com.example.calmscope", Context.MODE_PRIVATE);
+        db.resultsDao().insertResult(new Results(
+                db.usersDao().getIdByUsername(prefs.getString("currentUser","")),
+                db.emotionsDao().findByType("Calm").getId(),
+                new java.sql.Date(new java.util.Date().getTime()))
+        );
 
         Button backBtn = view.findViewById(R.id.goBackBtn);
         ImageView currentPhoto = view.findViewById(R.id.currentPhoto);
